9c00700c436cdffbc5fcfeb3d8baeb87e4341634
hakanai03 <hakanai.blog@gmail.com>
2023-03-21 13:41:01 +0900

    bugfix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { SlackEventMiddlewareArgs } from "@slack/bolt";
 import { WebClient } from "@slack/web-api";
+import { AccessoryElement } from "@slack/web-api/dist/response/ConversationsHistoryResponse";
+import { Block } from "@slack/web-api/dist/response/ConversationsOpenResponse";
+import { Accessory } from "@slack/web-api/dist/response/ConversationsRepliesResponse";
 import { getEnv } from "./lib/env";
 import { createCompletion, Messages } from "./lib/openai";
 import { makeSlackBot } from "./lib/slack";
@@ -18,6 +21,56 @@ const bot = makeSlackBot({
 
 const webClient = new WebClient(slackBotToken);
 
+const filterRichTextBlocks = (blocks?: Block[]): Block[] => {
+  if (!blocks) return [];
+  return blocks.filter((block) => block.type === "rich_text");
+};
+
+const filterElements = (accessories?: Accessory[]): Accessory[] => {
+  if (!accessories) return [];
+  return accessories.filter((accessory) =>
+    ["rich_text_section", "rich_text_preformatted", "rich_text_quote"].some(
+      (i) => i === accessory.type
+    )
+  );
+};
+
+type TextAccessoryElement = AccessoryElement & {
+  type: "text";
+  text: string;
+};
+
+const filterElementTexts = (
+  accessoryElements?: AccessoryElement[],
+  option?: {
+    code: boolean;
+  }
+): TextAccessoryElement[] => {
+  if (!accessoryElements) return [];
+  const textAccessoryElements = accessoryElements.filter(
+    (accessoryElement) => accessoryElement.type === "text"
+  ) as TextAccessoryElement[];
+  if (option?.code) {
+    return textAccessoryElements.map((elm) => ({
+      ...elm,
+      text: `\`\`\`${elm.text}\`\`\``,
+    }));
+  }
+  return textAccessoryElements;
+};
+
+const extractText = (elements: TextAccessoryElement[]) => {
+  return elements.reduce((acc, curr, index) => {
+    if (curr.type === "text") {
+      acc += curr.text;
+      if (index !== elements.length - 1) {
+        acc += "\n";
+      }
+    }
+    return acc;
+  }, "");
+};
+
 const appMentionHandler = async ({
   event,
   say,
@@ -33,34 +86,34 @@ const appMentionHandler = async ({
       });
       const { user_id } = await webClient.auth.test();
 
-      console.log("Thread messages:");
-      const message: Messages = result.messages
+      const messages = result.messages
         ?.map((msg) => {
-          const user = msg.user;
-          const element = msg.blocks?.[0].elements?.[0].elements?.find(
-            (el) => el.type === "text"
-          );
-          if (!user || !element) return;
-          const content = (element as { type: "text"; text: string }).text;
-
-          console;
-
-          return {
-            role: user === user_id ? "assistant" : "user",
-            content,
-          } as ArrayElement<Messages>;
+          const texts = filterRichTextBlocks(msg.blocks).flatMap((block) => {
+            return filterElements(block.elements).flatMap((element) => {
+              switch (element.type) {
+                case "rich_text_quote":
+                case "rich_text_preformatted":
+                  return filterElementTexts(element.elements, { code: true });
+                default:
+                  return filterElementTexts(element.elements);
+              }
+            });
+          });
+          const message: ArrayElement<Messages> = {
+            content: extractText(texts),
+            role: msg.user === user_id ? "assistant" : "user",
+          };
+          return message;
         })
-        .filter((i) => i !== undefined) as Messages;
-      console.log(message)
-
-      const completion = await createCompletion(message);
+        .filter((message) => message.content !== "");
+      if (!messages) return;
+      const completion = await createCompletion(messages);
 
       await webClient.chat.postMessage({
         channel,
         thread_ts,
         text: completion.data.choices.slice(-1)[0].message?.content,
-      })
-
+      });
     } catch (error) {
       console.error("Error fetching thread messages:", error);
     }
